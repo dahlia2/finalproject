9b93015ce5187e47ab03384ed65d6a5c29e25743
jck5049 <jck5049@naver,com>
2023-06-23 15:10:41 +0900

    class

--- halbae/src/main/java/com/gdu/halbae/controller/WishController.java
+++ halbae/src/main/java/com/gdu/halbae/controller/WishController.java
@@ -1,11 +1,15 @@
 package com.gdu.halbae.controller;
 
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.gdu.halbae.service.WishService;
 
@@ -27,28 +31,25 @@ public class WishController {
 		return "classlist/wish";
 	}
 	
-	// 찜목록 삽입
-	@GetMapping("/wish.do")
-	public String addWish(HttpServletRequest request, Model model) {
-		model.addAttribute("wishResult", wishService.getWishByNo(request));
-		System.out.println("찜목록 삽입 " + model);
-		return "classlist/wish";
+	// 클래스리스트에 찜목록 삽입/삭제
+	@ResponseBody
+	@GetMapping(value="/wish.do", produces="application/json")
+	public Map<String, Object> addWish(HttpServletRequest request) {
+		return wishService.getWishByNo(request);
 	}
 	
-	// 찜목록 삭제
+	// 찜목록 페이지 삭제
 	@GetMapping("/removeWish.do")
-	public String removeWish(HttpServletRequest request, Model model) {
-		model.addAttribute("removeResult", wishService.getWishRemoveByNo(request));
-		System.out.println("찜목록 삭제 " + model);
-		return "classlist/wish";
+	public String removeWish(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+		int removeResult = wishService.getWishRemoveByNo(request);
+		redirectAttributes.addFlashAttribute("removeResult", removeResult);
+		return "redirect:/wish/listWish.do";
 	}
 	
 	// 찜목록 리스트
 	@GetMapping("/listWish.do")
 	public String listWish(HttpServletRequest request, Model model) {
 		wishService.getWishList(request, model);
-		System.out.println("찜목록 리스트 리퀘스트 : " + request);
-		System.out.println("찜목록 리스트 모델 : " + model);
 		return "classlist/wish";
 	}
 	
