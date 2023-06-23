469876364ef8e647dcd88f7f49711860bf840354
parkhy0sh1n <parkhy0sh1n@naver.com>
2023-06-23 14:27:28 +0900

    완성

--- halbae/src/main/java/com/gdu/halbae/controller/ChatController.java
+++ halbae/src/main/java/com/gdu/halbae/controller/ChatController.java
@@ -3,14 +3,14 @@ package com.gdu.halbae.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.gdu.halbae.domain.ConversationDTO;
 import com.gdu.halbae.domain.MessageDTO;
@@ -26,65 +26,47 @@ public class ChatController {
         this.chatService = chatService;
     }
 
-	// 채팅방 생성
-    @PostMapping("/create")
-    public String createChatRoom(int classNo) {
-        // classNo를 기반으로 채팅방 생성
-        ConversationDTO conversationDTO = new ConversationDTO();
-        conversationDTO.setClassNo(classNo);
-        chatService.createChat(conversationDTO);
-        return "redirect:/chat/create";
+    @GetMapping("/list")
+    public String chatList(Model model) {
+        List<ConversationDTO> conversations = chatService.getConversationList();
+        model.addAttribute("conversations", conversations);
+        return "chat/chatList";
     }
     
-    // 채팅방 입장
-    @GetMapping("/enter")
-    public void enterChatRoom(@RequestParam("conId") int conId) {
-        chatService.enterChatRoom(conId);
+    @GetMapping("/list2")
+    public String chatList2(Model model) {
+        List<ConversationDTO> conversations = chatService.getConversationList();
+        model.addAttribute("conversations", conversations);
+        return "chat/chatList2";
     }
 
-    // 채팅방 퇴장
-    @GetMapping("/exit")
-    public void exitChatRoom(@RequestParam("conId") int conId) {
-        chatService.exitChatRoom(conId);
+    @GetMapping("/{conId}")
+    public String chatRoom(@PathVariable int conId, Model model) {
+        ConversationDTO conversation = chatService.getConversation(conId);
+        List<MessageDTO> messages = chatService.getMessageList(conId);
+        model.addAttribute("conversation", conversation);
+        model.addAttribute("messages", messages);
+        return "chat/chat";
     }
 
-    // 채팅 내용 가져오기
-    @GetMapping("/messages")
-    public List<MessageDTO> getChatMessages(@RequestParam("conId") int conId) {
-        return chatService.getChatMessagesByConversation(conId);
-    }
-    
-    // 메시지 전송
-    @PostMapping("/send")
-    public ResponseEntity<?> sendMessage(@RequestParam("conId") int conId,
-                                         @RequestParam("userNo") int userNo,
-                                         @RequestParam("message") String message) {
+    @PostMapping("/{conId}/send")
+    @ResponseBody
+    public String sendMessage(@PathVariable int conId, @RequestParam int userNo, @RequestParam String message) {
         chatService.sendMessage(conId, userNo, message);
-        return ResponseEntity.ok().build();
+        return "success";
     }
-    
-    // 메시지 실시간 업데이트(Long Polling 방식)
-    @GetMapping("/messages/{conId}")
-    public List<MessageDTO> getChatMessages(@PathVariable("conId") int conId,
-                                            @RequestParam("lastMsgId") int lastMsgId) {
-        return chatService.getNewChatMessages(conId, lastMsgId);
+
+    @PostMapping("/{conId}/exit")
+    @ResponseBody
+    public String exitChatRoom(@PathVariable int conId, @RequestParam int userNo) {
+        chatService.exitChatRoom(conId, userNo);
+        return "success";
     }
-    
-    // 채팅 내역 조회
-    @GetMapping("/history/{conId}")
-    public List<MessageDTO> getChatHistory(@PathVariable("conId") int conId) {
-        return chatService.getChatHistory(conId);
+
+    @PostMapping("/{conId}/delete")
+    @ResponseBody
+    public String deleteChatRoom(@PathVariable int conId) {
+        chatService.deleteChatRoom(conId);
+        return "success";
     }
-    
-    // 채팅방 삭제
-    @DeleteMapping("/delete/{conId}")
-    public ResponseEntity<String> removeChatRoom(@PathVariable("conId") int conId) {
-        boolean deleted = chatService.removeChatRoom(conId);
-        if (deleted) {
-            return ResponseEntity.ok("대화방이 삭제되었습니다.");
-        } else {
-            return ResponseEntity.badRequest().body("대화방 삭제를 실패하였습니다. 다시 시도해주세요.");
-        }
-    }
-    
 }
--- halbae/src/main/java/com/gdu/halbae/domain/ReviewDTO.java
+++ halbae/src/main/java/com/gdu/halbae/domain/ReviewDTO.java
@@ -1,7 +1,6 @@
 package com.gdu.halbae.domain;
 
 import java.util.Date;
-import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,11 +11,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ReviewDTO {
     private int reviewNo;
-    private int userNo;
     private int classNo;
     private String reviewContent;
     private double reviewGrade;
     private Date writeDate;
-    private List<ReviewAttachDTO> reviewAttachList;
     private UserDTO userDTO;
+    private int likes;
 }
