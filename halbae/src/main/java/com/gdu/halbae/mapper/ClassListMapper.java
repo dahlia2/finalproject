1e4c71e4af1f7f75a891bc33339aecc59903ddfd
jck5049 <jck5049@naver,com>
2023-06-25 18:48:13 +0900

    classList

--- halbae/src/main/java/com/gdu/halbae/mapper/ClassListMapper.java
+++ halbae/src/main/java/com/gdu/halbae/mapper/ClassListMapper.java
@@ -15,10 +15,11 @@ public interface ClassListMapper {
 	public int getClassListCountAll();										// 전체 클래스
 	public int getClassListCountNew();										// 최신 클래스
 	public List<ClassListDTO> selectClassListNew(Map<String, Object> map);	// 최신 클래스
-	public ImgClassDTO selectClassByNo(int classNo);						// 디테일 클래스
+	public ClassListDTO selectClassByNo(int classNo);						// 디테일 클래스
 	
 	public int insertClass(ClassListDTO classListDTO);						// 클래스 등록
 	public ClassListDTO getClassByNo(int classNo);							// 이미지 뽑을때 클래스 번호주고 해당 HDD저장되어있는 imgMainPath 가져오는 코드
 	public List<Integer> selectClassUploadList(int userNo);					// 클랙스 등록 목록 userNo주고 classNo받아오는 코드
 	public List<ClassListDTO> selectUploadList(List<Integer> uploadList);	// 다 뽑은 classNo를 전달해서 classList 테이블에 컬럼 값을 가져오는 코드!
+	public int removeClass(int classNo);									// 등록한 클래스 삭제
 }
--- halbae/src/main/java/com/gdu/halbae/service/ClassListService.java
+++ halbae/src/main/java/com/gdu/halbae/service/ClassListService.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.gdu.halbae.domain.ClassListDTO;
 import com.gdu.halbae.domain.ImgClassDTO;
@@ -12,9 +13,13 @@ import com.gdu.halbae.domain.ImgClassDTO;
 public interface ClassListService {
 	public void getClassList(HttpServletRequest request, Model model);		// 전체/카테고리 클래스
 	public void getClassListNew(HttpServletRequest request, Model model);	// 최신 클래스
-	public ImgClassDTO getClassByNo(HttpServletRequest request);			// 디테일 클래스
+	public ClassListDTO getClassByNo(HttpServletRequest request);			// 디테일 클래스
 	
 	public int addClass(MultipartHttpServletRequest multipartHttpServletRequest);	// 클래스 등록(파일)
-	public ResponseEntity<byte[]> display(int classNo);								// 클래스 등록한 이미지 뽑는 메소드 
+	public ResponseEntity<byte[]> display(int classNo);								// 클래스 등록한 이미지 뽑는 메소드(메인)
+	public ResponseEntity<byte[]> displayDetail(int classNo);						// 클래스 등록한 이미지 뽑는 메소드(상세) 
 	public void getClassUploadList(HttpServletRequest request, Model model);		// 클래스 등록 목록
+	public int getClassUploadRemove(HttpServletRequest request);					// 등록한 클래스 삭제
+	public void getClassEdit(HttpServletRequest request, Model model);				// 등록한 클래스 편집화면(값 화면에 뿌리기)
+	public int modifyClass(MultipartHttpServletRequest multipartHttpServletRequest);// 등록한 클래스 수정
 }
